c919e6ea6092e891feb532f2fdd1fdb118d0aaa7
Ben Hall <ben@benh.codes>
2022-01-24 18:15:08 +0000

    Added listening for when a guild disappears/kicks the bot
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -3,23 +3,15 @@ const Sentry = require('@sentry/node');
 const { eventLogger } = require('../utils/loggingUtils');
 
 module.exports = {
-	name: 'guildCreate',
+	name: 'guildDelete',
 	async execute(guild) {
 		try {
-			let dbGuild = await Guild.findByPk(guild.id);
-			if (dbGuild != null) {
-				dbGuild.active = true;
-				await dbGuild.save();
-			}
-			else {
-				dbGuild = await Guild.create({
-					id: guild.id,
-					name: guild.name,
-					premium: false,
-				});
-			}
+			const dbGuild = await Guild.findByPk(guild.id);
+			dbGuild.active = false;
+			await dbGuild.save();
+
 			guild.client.user.setActivity(`${guild.client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members in ${guild.client.guilds.cache.size} servers`, { type: 'WATCHING' });
-			eventLogger.info(`Joined a new guild '${guild.name}' (${guild.id}) with ${guild.memberCount} members`);
+			eventLogger.info(`Left a guild '${guild.name}' (${guild.id}) with ${guild.memberCount} members`);
 		}
 		catch (e) {
 			Sentry.setTag('guild_id', guild.id);

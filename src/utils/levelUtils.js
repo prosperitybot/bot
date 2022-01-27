63a1d983f6cc1190e58b4260fb47485247df1d4d
Ben Hall <ben@benh.codes>
2022-01-27 12:21:04 +0000

    [skip ci] Added submodule for translations
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/utils/levelUtils.js
+++ src/utils/levelUtils.js
@@ -1,5 +1,5 @@
 module.exports = {
 	getXpNeeded: (level) => {
-		return 5 / 6 * level * (2 * level * level + 27 * level + 91);
+		return Math.ceil(5 / 6 * level * (2 * level * level + 27 * level + 91));
 	},
 };

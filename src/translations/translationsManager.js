af7aaad92f59bc2b682c9fcefd075a8c9d38942f
Ben Hall <ben@benh.codes>
2022-02-12 18:13:10 +0000

    Fixed translation manager
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/translations/translationsManager.js
+++ src/translations/translationsManager.js
@@ -2,12 +2,15 @@ const { Guild } = require('@prosperitybot/database');
 
 module.exports = {
   get: async (guildId, client) => {
+    let locale;
     if (client.guildTranslations.get(guildId) === undefined) {
       const guild = await Guild.findByPk(guildId);
       console.log(guild.locale);
       client.guildTranslations.set(guildId, guild.locale);
+      locale = guild.locale;
+    } else {
+      locale = client.guildTranslations.get(guildId);
     }
-    const locale = client.guildTranslations.get(guildId);
     console.log(locale);
     console.log(client.translations.get(locale));
     return client.translations.get(locale);

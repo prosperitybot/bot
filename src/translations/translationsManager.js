e4b7bd0e72513ee77f7a648093e3834a9489d347
Ben Hall <ben@benh.codes>
2022-02-12 18:30:49 +0000

    Fixed translation manager
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/translations/translationsManager.js
+++ src/translations/translationsManager.js
@@ -5,14 +5,11 @@ module.exports = {
     let locale;
     if (client.guildTranslations.get(guildId) === undefined) {
       const guild = await Guild.findByPk(guildId);
-      console.log(guild.locale);
       client.guildTranslations.set(guildId, guild.locale);
       locale = guild.locale;
     } else {
       locale = client.guildTranslations.get(guildId);
     }
-    console.log(locale);
-    console.log(client.translations.get(locale));
     return client.translations.get(locale);
   },
   format: (str, format) => {

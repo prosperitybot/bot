6a36b92f8408526cb27bf2aba34ea3b92e199de1
Ben Hall <ben@benh.codes>
2022-03-04 23:11:14 +0000

    chore: Fix typescript
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/commands/Language.ts
+++ src/commands/Language.ts
@@ -1,5 +1,5 @@
 import {
-  BaseCommandInteraction, Client, MessageActionRow, MessageButton,
+  Client, CommandInteraction, MessageActionRow, MessageButton,
 } from 'discord.js';
 import { Command } from '../typings/Command';
 import { LogInteractionError } from '../managers/ErrorManager';
@@ -14,25 +14,26 @@ const Language: Command = {
   ownerOnly: false,
   description: 'Shows current language & language settings for both user and servers',
   type: 'CHAT_INPUT',
-  run: async (client: Client, interaction: BaseCommandInteraction) => {
+  run: async (client: Client, interaction: CommandInteraction) => {
     try {
-      const translations = await GetTranslations(interaction.user.id, interaction.guildId!);
-      const buttonRow = new MessageActionRow()
-        .addComponents(
-          new MessageButton()
-            .setLabel(translations.commands.language.server_language)
-            .setStyle('DANGER')
-            .setCustomId('language-server_menu')
-            .setEmoji('🖥️')
-            .setDisabled(!HasPermission(interaction.member, 'ADMINISTRATOR')),
-          new MessageButton()
-            .setLabel(translations.commands.language.user_language)
-            .setStyle('SUCCESS')
-            .setEmoji('🧍')
-            .setCustomId('language-user_menu'),
-        );
-
-      await ReplyToInteraction(interaction, translations.commands.language.choice_message, true, [buttonRow]);
+      if (interaction.inCachedGuild()) {
+        const translations = await GetTranslations(interaction.user.id, interaction.guildId!);
+        const buttonRow = new MessageActionRow()
+          .addComponents(
+            new MessageButton()
+              .setLabel(translations.commands.language.server_language)
+              .setStyle('DANGER')
+              .setCustomId('language-server_menu')
+              .setEmoji('🖥️')
+              .setDisabled(!HasPermission(interaction.member, 'ADMINISTRATOR')),
+            new MessageButton()
+              .setLabel(translations.commands.language.user_language)
+              .setStyle('SUCCESS')
+              .setEmoji('🧍')
+              .setCustomId('language-user_menu'),
+          );
+        await ReplyToInteraction(interaction, translations.commands.language.choice_message, true, [buttonRow]);
+      }
       return;
     } catch (e) {
       await LogInteractionError(e, interaction);

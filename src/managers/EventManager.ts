57558a363d1f6146e241ab6610e4ffbdf6b9d06b
Ben Hall <ben@benh.codes>
2022-03-05 10:21:59 +0000

    fix: users were not gaining xp
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/managers/EventManager.ts
+++ src/managers/EventManager.ts
@@ -5,11 +5,13 @@ import GuildCreateEvent from '../events/GuildCreateEvent';
 import GuildDeleteEvent from '../events/GuildDeleteEvent';
 import InteractionCreateEvent from '../events/InteractionCreateEvent';
 import ReadyEvent from '../events/ReadyEvent';
+import MessageEvent from '../events/MessageEvent';
 
 const Events: Event[] = [
   GuildCreateEvent,
   GuildDeleteEvent,
   InteractionCreateEvent,
+  MessageEvent,
   ReadyEvent,
 ];
 

dc32a9e6c50b2296593168f4570f5301401d8e2d
Ben Hall <ben@benh.codes>
2022-03-05 08:38:59 +0000

    ci: Fixed bug with dockerfile
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ async function start() {
     whitelabelBots.forEach(async (bot: WhitelabelBot) => {
       AddClient(bot.botId, await Bot(bot.token));
     });
-  });
+  }); 
 
   const mainBot: Client = await Bot(process.env.DISCORD_TOKEN!);
   AddClient(process.env.CLIENT_ID!, mainBot);

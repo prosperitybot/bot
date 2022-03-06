2ae0909102a87e74f0265a12bb6e79900e4c88be
Ben Hall <ben@benh.codes>
2022-03-06 06:59:51 +0000

    fix: updated intents
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/Bot.ts
+++ src/Bot.ts
@@ -5,7 +5,6 @@ const Login = async (token: string): Promise<Client> => {
   const client = new Client({
     intents: [
       Intents.FLAGS.GUILDS,
-      Intents.FLAGS.GUILD_MESSAGES,
     ],
   });
 

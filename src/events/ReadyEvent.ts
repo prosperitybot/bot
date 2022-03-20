d42a932f336712b6070ed04a2109f3c87129e74a
Ben Hall <ben@benh.codes>
2022-03-20 12:06:05 +0000

    fix: Whitelabel
    Signed-off-by: Ben Hall <ben@benh.codes>

--- src/events/ReadyEvent.ts
+++ src/events/ReadyEvent.ts
@@ -1,9 +1,10 @@
 import { Client } from 'discord.js';
-// import { ActivityTypes } from 'discord.js/typings/enums';
+import { WhitelabelBot } from '@prosperitybot/database';
+import { ActivityTypes } from 'discord.js/typings/enums';
 import { LogClientError } from '../managers/ErrorManager';
 import Commands from '../managers/CommandManager';
-
 import { Event } from '../typings/Event';
+import { IsWhitelabel } from '../managers/ClientManager';
 
 const ReadyEvent: Event = {
   name: 'ready',
@@ -13,7 +14,12 @@ const ReadyEvent: Event = {
       // eslint-disable-next-line no-console
       console.log(`Logged in as ${client.user?.username}#${client.user?.discriminator}`);
       await client.application?.commands.set(Commands.map((c) => c.data));
-      // client.user?.setActivity({ name: '/about', type: ActivityTypes.LISTENING });
+      if (IsWhitelabel(client)) {
+        const wlBot: WhitelabelBot = await WhitelabelBot.findOne({ where: { botId: client.application?.id } });
+        client.user?.setActivity({ name: wlBot.statusContent, type: wlBot.statusType });
+      } else {
+        client.user?.setActivity({ name: '/about', type: ActivityTypes.LISTENING });
+      }
     } catch (e) {
       await LogClientError(e, client);
     }
